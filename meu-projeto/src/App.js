8045aceb70656993eaa7aa015f3294bea986934f
Pablo <lucasrn03@gmail.com>
2023-08-24 15:39:33 -0300

    Entendendo o JSX e Criando Componentes

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
+
+  const name = "Pablo"
+  const nameUpper = name.toUpperCase()
+  const age1 = 20
+  const age2 = 21
+  const url = "https://img.freepik.com/fotos-gratis/close-up-no-lindo-gato_23-2149216326.jpg"
+
+  function soma (age1, age2){
+    return age1 + age2
+  }
+
   return (
     <div className="App">
-      <h1>Olá Mundo</h1>
+      <h1>Alterando o JSX</h1>
+      <p>Olá {nameUpper}</p>
+      <p>Soma = {soma(age1, age2)}</p>
+      <img src={url} alt="gato"/>
+      <HelloWorld />
     </div>
   );
 }
